33f1b8ee0863f9a3135af7776f78c6df461cd094
Ola <aleksandra.karp1@gmail.com>
2020-04-05 11:52:16 +0200

    little changes in input, h3, h4

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ class App extends React.Component {
   state = {
     omens: ['zapomnisz, jak to było mieć na sobie dżins', 'przytyjesz 10kg', 'nabawisz się kontuzji kciuka od scrollowania', 'nadrobisz książki i filmy, na które nie miałeś czasu', 'odkryjesz, że ryż z groszkiem to też obiad', 'ogarniesz React.js', 'dokończysz rozgrzebane projekty', 'wyśpisz się', 'będziesz ćwiczyć z Chodakowską', 'poczujesz magię pracy zdalnej', 'będziesz jadł pizzę 3 razy w tygodniu'],
     omen: null,
-    newOmen: null
+    newOmen: ''
   }
 
   randomOmenHandler = () => {
@@ -22,9 +22,10 @@ class App extends React.Component {
   addOmenHandler = () => {
     const omens = this.state.omens;
     let newOmen = this.state.newOmen;
-    if(newOmen!=null){
+    if(newOmen!=''){
     this.setState({
-      omens: omens.concat(newOmen)
+      omens: omens.concat(newOmen),
+      newOmen: ''
     })
     alert(`Dodałeś wróżbę "${newOmen}". Ciekawe, czy jest Ci pisana! Dowiedz się za pomocą przycisku "losuj wróżbę".`)
     }else{
@@ -40,8 +41,10 @@ class App extends React.Component {
         <h2>Możesz także dodać swoje opcje!</h2>
         <input type="text" placeholder="wpisz swoją wróżbę" value={this.state.newOmen} onChange={this.handleChange}/>
         <button class="addOmen" onClick={this.addOmenHandler}>Dodaj wróżbę</button>
-        <h3>{this.state.omen}</h3>
-        <h1 class='zostanwdomu'>#zostan<span>w</span>domu</h1>
+        <div className="omenContainer">
+          {this.state.omen ? <h3>{this.state.omen}</h3> : null}
+        </div>
+        <h4 class='zostanwdomu'>#zostan<span>w</span>domu</h4>
       </div>
     );
   }
